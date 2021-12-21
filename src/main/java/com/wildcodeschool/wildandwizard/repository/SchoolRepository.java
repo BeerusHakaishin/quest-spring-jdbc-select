5ed9811ec2d8568df33a277da66b9b7d161d48c4
Ismaël Meziani <meziani-i@outlook.fr>
2021-12-21 17:32:53 +0100

    good

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,30 +1,109 @@
 package com.wildcodeschool.wildandwizard.repository;
 
-import com.wildcodeschool.wildandwizard.entity.School;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.wildcodeschool.wildandwizard.entity.School;
 
 public class SchoolRepository {
 
-    private final static String DB_URL = "jdbc:mysql://localhost:3306/spring_jdbc_quest?serverTimezone=GMT";
-    private final static String DB_USER = "h4rryp0tt3r";
-    private final static String DB_PASSWORD = "Horcrux4life!";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/spring_jdbc_quest?serverTimezone=GMT";
+    private static final String DB_USER = "h4rryp0tt3r";
+    private static final String DB_PASSWORD = "Horcrux4life!";
 
     public List<School> findAll() {
+    	   Connection connection = null;
+           PreparedStatement statement = null;
+           ResultSet resultSet = null;
+         
+           try {
+               connection = DriverManager.getConnection(
+                       DB_URL, DB_USER, DB_PASSWORD
+               );
+               statement = connection.prepareStatement(
+                    "SELECT * FROM school;"
+            );
+            resultSet = statement.executeQuery();
+            List<School> schools = new ArrayList<>();
+            
+          
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                Long capacity = resultSet.getLong("capacity");
+                String country = resultSet.getString("country");
+                schools.add(new School(id, name, capacity, country));
+            }
+            return schools;
 
-        // TODO : find all schools
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     public School findById(Long id) {
+    	
+    	 Connection connection = null;
+         PreparedStatement statement = null;
+         ResultSet resultSet = null;
 
-        // TODO : find a school by id
+        try {
+             connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            statement = connection.prepareStatement(
+                    "SELECT * FROM school WHERE id = ?;"
+            );
+            statement.setLong(1, id);
+            resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                String name = resultSet.getString("name");
+                Long capacity = resultSet.getLong("capacity");
+                String country = resultSet.getString("country");
+                return new School(id, name, capacity, country);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     public List<School> findByCountry(String country) {
+    	
+    	 Connection connection = null;
+         PreparedStatement statement = null;
+         ResultSet resultSet = null;
 
-        // TODO : search schools by country
+        try {
+            connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            statement = connection.prepareStatement(
+                    "SELECT * FROM school WHERE country = ?;"
+            );
+            statement.setString(1, country);
+            List<School> schools = new ArrayList<>();
+            resultSet = statement.executeQuery();
+            
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                Long capacity = resultSet.getLong("capacity");
+                schools.add(new School(id, name, capacity, country));
+            }
+            return schools;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
